4fae26d4febfdc9cb416ac239546df98a57aad77
Jayant Shrivastava <jshrivastava03@gmail.com>
2020-04-14 19:41:44 -0400

    Added cache to autocompleter

--- autocomplete/autocompleter.py
+++ autocomplete/autocompleter.py
@@ -1,12 +1,23 @@
 from autocomplete.trie import Trie
+from autocomplete.lrucache import LRUCache
 from typing import List
 
 class Autocompleter:
     def __init__(self, capacity: int, phrases: List[str], frequencies: List[int]):
+        assert capacity >= len(phrases), "Cannot insert more phrases than capacity"
         self.trie = Trie(phrases, frequencies)
+        self.cache = LRUCache(capacity, phrases)
 
     def search(self, phrase: str, results_size: int) -> List[str]:
         return self.trie.search(phrase, results_size)
 
-    def insert(self, phrase: str, freq: int = 1) -> List[str]:
-        return self.trie.insert(phrase, freq)
+    def commit(self, phrase: str, freq: int = 1):
+        assert phrase != "", "Should not insert an empty string"
+
+        self.trie.insert(phrase, freq)
+        removed = self.cache.put(phrase)
+        if removed:
+            self.trie.delete(removed)
+        return removed
+         
+    
--- tests/test_autocompleter.py
+++ tests/test_autocompleter.py
@@ -2,7 +2,13 @@ import unittest
 
 from autocomplete import Autocompleter
 
-class TestAutocompleter(unittest.TestCase):
+class c(unittest.TestCase):
+
+    def test_constructor_throws_insufficient_capacity(self):
+        self.assertRaises(AssertionError, Autocompleter, 0, ['test'], [1])
+    
+    def test_constructor_throws_length_mismatch(self):
+        self.assertRaises(AssertionError, Autocompleter, 0, ['test'], [1, 2])
 
     def test_search_results(self):
         capacity = 10
@@ -11,10 +17,21 @@ class TestAutocompleter(unittest.TestCase):
         autocompleter = Autocompleter(capacity, phrases, frequencies)
 
         self.assertEqual(autocompleter.search('auto', 4), ['autocompletion library', 'autocomplete', 'autocompletion', 'autocompleter'])
+        self.assertEqual(autocompleter.search('', 1), ['autocompletion library'])
 
     def test_search_results(self):
         autocompleter = Autocompleter(10, [], [])
-        autocompleter.insert("test")
+        autocompleter.commit("test")
         self.assertEqual(autocompleter.search('te', 1), ['test'])
-        autocompleter.insert("testing", 2)
+        autocompleter.commit("testing", 2)
         self.assertEqual(autocompleter.search('te', 2), ['testing', 'test'])
+
+    def test_lru_eviction(self):
+        capacity = 4
+        phrases = ['autocomplete', 'autocompletion', 'autocompleter', 'autocompletion library']
+        frequencies = [1, 2, 3, 4]
+        autocompleter = Autocompleter(capacity, phrases, frequencies)
+        self.assertEqual(autocompleter.commit("test"), 'autocomplete') # returns the lru value
+        self.assertEqual(autocompleter.commit("testing", 2), 'autocompletion') # returns the lru value
+        
+        self.assertEqual(autocompleter.search('', 4), ['autocompletion library', 'autocompleter', 'testing', 'test'])
